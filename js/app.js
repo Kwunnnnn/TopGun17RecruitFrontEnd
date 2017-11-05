9c432b9dea0ae2a7f5c1f033184eb40e76bbbdb3
Kanin Manoch <dietyboy@gmail.com>
2017-11-05 20:26:01 +0700

    a

--- js/app.js
+++ js/app.js
@@ -37,6 +37,63 @@
         }
     });
 
+    myApp.controller('QuestionController',function ($scope, $http) {
+        var user_id = localStorage.getItem('user_id');
+        $http.get("http://172.20.40.172:3000/problem", {
+        }).then(function successCallback(response) {
+            $scope.questions = response.data;
+            console.log($scope.questions);
+            for(var i = 0; i < $scope.questions.length; ++i){
+                $scope.questions[i].solved = false;
+                if(typeof $scope.questions[i].user_id_solved !== 'undefined'){
+                    for(var j = 0; j < $scope.questions[i].user_id_solved.length; ++j){
+                        if(user_id === $scope.questions[i].user_id_solved[j] ){
+                            $scope.questions[i].solved = true;
+                        }
+                    }
+                }
+            }
+        }, function errorCallback(response) {
+            console.log(response.data);
+        });
+
+        
+        $scope.solveProblem = function (id) {
+            sessionStorage.setItem('id', id);
+            window.location.href = "problem.html";
+        }
+    });
+
+    myApp.controller('ProblemController',function ($scope, $http) {
+        $scope.showFile = true;
+        $http.get("http://172.20.40.172:3000/problem/" + sessionStorage.getItem('id'), {
+        }).then(function successCallback(response) {
+            $scope.problem = response.data;
+            if($scope.problem.problems_file === ""){
+                $scope.showFile = false;
+            }
+        }, function errorCallback(response) {
+            console.log(response.data);
+        });
+        
+        $scope.submit = function () {
+            var problem_solve = {problem_id: $scope.problem._id, user_id: localStorage.getItem('user_id')};
+            if($scope.answer === $scope.problem.problems_result){
+                $http.patch("http://172.20.40.172:3000/problem/answer", angular.toJson(problem_solve), {
+                }).then(function successCallback(response) {
+                    window.location.href = "question.html";
+                }, function errorCallback(response) {
+                    console.log(response.data);
+                });
+            }
+            else{
+                alert("ตอบผิดนะครับ");
+            }
+        }
+        
+        
+
+    });
 
 
 
